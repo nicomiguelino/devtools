2f0b97a90fd7d26f339faf16bc2c92ef93f58d64
Nico Miguelino <nicomiguelino2014@gmail.com>
2024-11-10 18:24:50 -0800

    chore: add a step to install Docker (#4)

--- scripts/x86/install.sh
+++ scripts/x86/install.sh
@@ -171,6 +171,38 @@ function configure_oh_my_tmux() {
     wget -O $HOME/$CONFIG_FILE $DOWNLOAD_URL
 }
 
+function install_docker() {
+    local SIGNED_BY="/etc/apt/keyrings/docker.asc"
+    local DOCKER_DOWNLOAD_URL="https://download.docker.com/linux/debian/gpg"
+    local ARCH="$(dpkg --print-architecture)"
+
+    # Add Docker's official GPG key:
+    sudo apt-get update -y
+    sudo apt-get install -y ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg \
+        -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$ARCH signed-by=$SIGNED_BY] $DOCKER_DOWNLOAD_URL \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update -y
+
+    # Install Docker:
+    sudo apt-get install -y \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io \
+        docker-buildx-plugin \
+        docker-compose-plugin
+
+    # Add the current user to the docker group:
+    sudo usermod -aG docker $USER
+}
+
 function main() {
     install_prerequisites && clear
     source $HOME/.bashrc
@@ -197,6 +229,14 @@ function main() {
 
     display_section "Configure Oh My Tmux"
     configure_oh_my_tmux
+
+    display_section "Install Docker"
+    install_docker
+
+    display_section "Installation Complete"
+    gum style "Please restart your shell to apply changes." \
+        --foreground "212" | \
+        gum format
 
     echo
 }
