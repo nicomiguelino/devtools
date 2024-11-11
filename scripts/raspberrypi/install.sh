97b96d4d4b9ffcb0d8b4a00c51de4d00adcb62ea
nicomiguelino <nicomiguelino2014@gmail.com>
2024-11-10 18:52:06 -0800

    fix: docker installation

--- scripts/raspberrypi/install.sh
+++ scripts/raspberrypi/install.sh
@@ -186,24 +186,19 @@ function configure_oh_my_tmux() {
 }
 
 function install_docker() {
-    local SIGNED_BY="/etc/apt/keyrings/docker.asc"
-    local DOCKER_DOWNLOAD_URL="https://download.docker.com/linux/debian/gpg"
-    local ARCH="$(dpkg --print-architecture)"
-
     # Add Docker's official GPG key:
-    sudo apt-get update -y
-    sudo apt-get install -y ca-certificates curl
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg \
-        -o /etc/apt/keyrings/docker.asc
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources:
     echo \
-    "deb [arch=$ARCH signed-by=$SIGNED_BY] $DOCKER_DOWNLOAD_URL \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update -y
+    sudo apt-get update
 
     # Install Docker:
     sudo apt-get install -y \
